8ea73d935c59afabf5880b39f252e68e6705ca46
unclebusy <deniskalagin@gmail.com>
2024-07-25 16:54:40 +0600

    Updated Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Button, Container, Typography, Box } from '@mui/material';
 
+const clientId = 'Ov23liNCaE4oo7EGfH0D';
+const redirectUri = 'http://localhost:3000/oauth/callback';
+
+const loginWithGitHub = () => {
+  window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=user,repo`;
+};
+
 const Login = () => {
-  const clientId = 'Ov23liNCaE4oo7EGfH0D';
-  const redirectUri = 'http://localhost:3000/oauth/callback';
-
-  const loginWithGitHub = () => {
-    window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=user,repo`;
-  };
-
   return (
       <Container maxWidth="sm" style={{ display: 'flex', height: '100vh', alignItems: 'center', justifyContent: 'center' }}>
         <Box
@@ -20,19 +20,19 @@ const Login = () => {
               textAlign: 'center',
             }}
         >
-          <Typography
-              variant="h4"
-              component="h1"
-              gutterBottom
-              sx={{
-                color: '#1C2128',
-                padding: '4px 16px',
-                borderRadius: '4px',
-                display: 'inline-block'
-              }}
-          >
-            GitHub Dashboard
-          </Typography>
+          <Box>
+            <Typography
+                variant="h4"
+                component="h1"
+                gutterBottom
+                sx={{
+                  color: '#22272E',
+                  display: 'inline-block',
+                }}
+            >
+              GitHub Dashboard
+            </Typography>
+          </Box>
           <Box mt={1}>
             <Button variant="contained" color="primary" onClick={loginWithGitHub}>
               Login with GitHub
