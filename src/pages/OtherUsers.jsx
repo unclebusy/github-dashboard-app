63a85d0fec22effae4e1e4874148435657dc2521
unclebusy <deniskalagin@gmail.com>
2024-08-01 12:07:41 +0600

    Add CardUser.jsx

--- src/pages/OtherUsers.jsx
+++ src/pages/OtherUsers.jsx
@@ -1,8 +1,23 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { TextField, Button, List, ListItem, ListItemText, Typography, Container, Link, CircularProgress, Box } from '@mui/material';
-import Toolbar from "@mui/material/Toolbar";
-import AppBar from "@mui/material/AppBar";
+import {
+  TextField,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  Container,
+  Link,
+  CircularProgress,
+  Box,
+  Avatar
+} from '@mui/material';
+import PaperWrapper from "../components/PaperWrapper";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import InboxIcon from "@mui/icons-material/Inbox";
+import CardUser from "../components/CardUser";
 
 const UserSearch = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -16,9 +31,10 @@ const UserSearch = () => {
     const response = await axios.get('https://api.github.com/search/users', {
       params: {
         q: searchTerm,
-        per_page: 10
+        per_page: 27
       }
     });
+    console.log(response.data.items)
     setUsers(response.data.items);
     setTotalCount(response.data.total_count);
     setLoading(false);
@@ -47,32 +63,36 @@ const UserSearch = () => {
               onChange={(e) => setSearchTerm(e.target.value)}
               placeholder="Search users..."
               onKeyDown={handleKeyPress}
-              sx={{ marginRight: '1rem', marginTop: '1rem', marginBottom: '1rem' }}
+              sx={{ marginRight: '1rem', marginTop: '1rem', }}
           />
 
           <Button variant="contained" color="primary" onClick={handleSearch}>
             Search
           </Button>
         </Box>
-        {loading && <CircularProgress />}
-            {totalCount ?
-                <AppBar  position="static" sx={{ borderRadius: '4px' }}>
-                  <Toolbar>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                      Found {totalCount} users
-                    </Typography>
-                  </Toolbar>
-                </AppBar>
-                :
-                ''}
+        {loading ? <CircularProgress /> : (
+            totalCount ? (
+                <Box>
+                  <PaperWrapper padding='1rem' marginTop="1rem">
+                    <PaperWrapper padding="1rem" marginTop="0" bgColor="#1976D2">
+                      <Typography color="white" variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Found {totalCount} users
+                      </Typography>
+                    </PaperWrapper>
+                    <Box>
+                      <nav aria-label="Count of users found">
+                        <List>
+                          {users.map(user => (
+                              <CardUser key={user.id} id={user.id} login={user.login} fetchUserRepos={fetchUserRepos}/>
+                          ))}
+                        </List>
+                      </nav>
+                    </Box>
+                  </PaperWrapper>
+                </Box>
+            ) : ''
+        )}
 
-          <List>
-            {users.map(user => (
-                <ListItem key={user.id} onClick={() => fetchUserRepos(user.login)}>
-                  <ListItemText primary={user.login} />
-                </ListItem>
-            ))}
-          </List>
         <Typography variant="h6">Repositories:</Typography>
         <List>
           {repos.map(repo => (
@@ -88,4 +108,3 @@ const UserSearch = () => {
 };
 
 export default UserSearch;
-
