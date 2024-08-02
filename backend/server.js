fd9213f33476f79207a56ca15597b54db893d87c
unclebusy <deniskalagin@gmail.com>
2024-08-02 20:30:23 +0600

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const port = 4000;
+const port = process.env.PORT || 4000;
 require('dotenv').config();
 
 const app = express();
